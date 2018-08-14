22bbcb54f523f9e9cbd1403124aa4b74acc5fbff
Jonathan Solomon <jonathansolomon@Jonathans-iMac.local>
2018-08-13 21:13:30 -0400

    Init commit

---
+++ src/main/java/com/example/demo/Demoproj0Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoproj0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoproj0Application.class, args);
+	}
+}
